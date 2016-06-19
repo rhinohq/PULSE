9a582297b430b3c1fb944a7423f30d3d2e1fb97b
Ryan Kendrick <ryan@kendrickfamily.info>
2016-06-19 20:32:23 +0100

    Created PULSE.Android Client Project

---
+++ Android/PULSE/PULSE/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PULSE
+{
+	[Activity(Label = "PULSE", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
+
