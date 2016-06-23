97c8435d6cdb76aed6c503cd8c1ad66eef759295
Ryan Kendrick <ryan@kendrickfamily.info>
2016-06-23 16:48:38 +0100

    Added Wifi Functionality Core.Device Class

--- Android/PULSE/PULSE/Functionality/Core.cs
+++ Android/PULSE/PULSE/Functionality/Core.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 using Android.Content;
 using Android.Bluetooth;
+using Android.Net;
 using Android.Provider;
 using Android.Widget;
 
@@ -16,16 +18,70 @@ namespace PULSE
 	{
 		public static class Device
 		{
-			public static void TurnBluetoothOn()
-			{
-				Intent EnableIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
-				CrossCurrentActivity.Current.Activity.StartActivityForResult(EnableIntent, 2);
+			public static class Bluetooth
+			{ 
+				public static void TurnBluetoothOn()
+				{
+					Intent EnableIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
+					CrossCurrentActivity.Current.Activity.StartActivityForResult(EnableIntent, 2);
+				}
+
+				public static void MakeBluetoothDiscoverable()
+				{
+					Intent DiscoverableIntent = new Intent(BluetoothAdapter.ActionRequestDiscoverable);
+					CrossCurrentActivity.Current.Activity.StartActivityForResult(DiscoverableIntent, 2);
+				}
 			}
 
-			public static void MakeBluetoothDiscoverable()
+			public static class WiFi
+			{
+				static Android.Net.Wifi.WifiManager WifiMan = (Android.Net.Wifi.WifiManager)CrossCurrentActivity.Current.Activity.GetSystemService("wifi");
+
+				public static void ToggleWiFi(bool OnOff)
+				{
+					if (OnOff)
+						WifiMan.SetWifiEnabled(true);
+					else
+						WifiMan.SetWifiEnabled(false);
+				}
+
+				public static void ConnectToNework(string SSID, string Key)
+				{ 
+					var WifiConfig = new Android.Net.Wifi.WifiConfiguration();
+
+					WifiConfig.Ssid = string.Format("\"{0}\"", SSID);
+					WifiConfig.PreSharedKey = string.Format("\"{0}\"", Key);
+
+					ToggleWiFi(true);
+
+					int NetID;
+					var Network = WifiMan.ConfiguredNetworks.FirstOrDefault(cn => cn.Ssid == SSID);
+					90
+					if (Network != null)
+						NetID = Network.NetworkId;
+					else 
+					{
+						NetID = WifiMan.AddNetwork(WifiConfig);
+
+						WifiMan.SaveConfiguration();
+					}
+
+					WifiMan.UpdateNetwork(WifiConfig);
+
+					var CurrCon = WifiMan.ConnectionInfo;
+
+					if (CurrCon != null && CurrCon.NetworkId != NetID)
+					{
+						WifiMan.Disconnect();
+						WifiMan.EnableNetwork(NetID, true);
+						WifiMan.Reconnect();
+					}
+				}
+			}
+
+			public static class GPS
 			{ 
-				Intent DiscoverableIntent = new Intent(BluetoothAdapter.ActionRequestDiscoverable);
-				CrossCurrentActivity.Current.Activity.StartActivityForResult(DiscoverableIntent, 2);
+				
 			}
 		}
 
