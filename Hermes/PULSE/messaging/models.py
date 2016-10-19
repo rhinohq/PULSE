43fbfd811a8e994841f3dd6527d97897b4493ec8
Ryan Kendrick <ryan@kendrickfamily.info>
2016-10-19 18:23:44 +0100

    Changed Public Token's max_length

--- Hermes/PULSE/messaging/models.py
+++ Hermes/PULSE/messaging/models.py
@@ -3,13 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Client(models.Model):
-    public_token = models.CharField(max_length=30)
+    public_token = models.CharField(max_length=24)
     private_token = models.CharField(max_length=7)
     name = models.CharField(max_length=None)
     playerid = models.CharField(max_length=30)
 
 class Device(models.Model):
-    public_token = models.CharField(max_length=30)
+    public_token = models.CharField(max_length=24)
     private_token = models.CharField(max_length=7)
     name = models.CharField(max_length=None)
     address = models.GenericIPAddressField()
