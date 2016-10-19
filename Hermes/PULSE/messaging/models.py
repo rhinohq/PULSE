89747a30471b670837e09b751710b3b7f3a676f6
Ryan Kendrick <ryan@kendrickfamily.info>
2016-10-19 17:53:32 +0100

    Server 3 Renamed to 'Hermes'

---
+++ Hermes/PULSE/messaging/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Client(models.Model):
+    public_token = models.CharField(max_length=30)
+    private_token = models.CharField(max_length=7)
+    name = models.CharField(max_length=None)
+    playerid = models.CharField(max_length=30)
+
+class Device(models.Model):
+    public_token = models.CharField(max_length=30)
+    private_token = models.CharField(max_length=7)
+    name = models.CharField(max_length=None)
+    address = models.GenericIPAddressField()
